0afd0f18b94073ecf293b88f7da64e5dfa222bdc
ray <raymonhardy@gmail.com>
2018-03-08 12:05:00 -0700

    Initial Commit

---
+++ 8Queens/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  8Queens
+//
+//  Created by Raymon Hardy on 3/8/18.
+//  Copyright © 2018 Raymon Hardy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
